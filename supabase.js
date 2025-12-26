30ba5a866cbd29428e739cb4fcc4c9887b251f1e
adriaanisufi-ops <adriaanisufi@gmail.com>
2025-12-26 23:58:11 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
+// supabase.js
 const SUPABASE_URL = "https://mzlvftetsjnpbnqncwzg.supabase.co";
 const SUPABASE_KEY = "sb_publishable_6JUFHg9mRZHA0iOiDqI0vA_YaF4dvaY";
 
-window.supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+// Maak client en zet hem op window zodat al je pagina's hem kunnen gebruiken
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
